af3fe2ee9d891aa922b8a8c1ee60e8d930aa8014
bossug <work_mobiles@list.ru>
2018-10-30 12:21:17 +0300

    delete to pt

--- pt/pt.catalog/lang/ru/.parameters.php
+++
@@ -1,6 +0,0 @@
-<?
-$MESS['T_IBLOCK_DESC_LIST_TYPE']='Тип Инфоблока';
-$MESS['T_IBLOCK_DESC_LIST_ID']='ID инфоблока';
-$MESS['SECTION_PAGE']='Раздел';
-$MESS['DETAIL_PAGE']='Детальная';
-$MESS['IBLOCK_ACTIONS']='**';
